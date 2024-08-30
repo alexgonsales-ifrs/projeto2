e8c4b6908fc37ecb3640c3ba5aea565d7b92a894
alex <>
2024-08-30 14:50:53 -0300

    Commit 001 alex: primeiro commit.

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Projeto2.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Projeto2.X.production.hex
